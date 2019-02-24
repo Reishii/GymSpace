eff0a6797cad654b55fd6de5ee3c4978ad01973e
Rolly Lacap <lacaprjc@gmail.com>
2019-02-24 14:37:26 -0800

    Changed logic between the me and profile classes. The profile widget is now a stateful widget that contains most of what the me tab contained. The me widget now builds the profile.

--- gym_space/lib/logic/profile.dart
+++ gym_space/lib/logic/profile.dart
@@ -1,8 +1,17 @@
 import 'user.dart';
 import 'challenge.dart';
 import 'post.dart';
+import 'package:flutter/material.dart';
 
-class Profile {
+
+class Profile extends StatefulWidget {
+  User forUser;
+  
+  Profile(this.forUser);
+  _ProfileState createState() => _ProfileState(forUser);
+}
+
+class _ProfileState extends State<Profile> {
   String _description;
   String _quote;
   String _avatarImage; // changed from UML Image -> String
@@ -12,16 +21,132 @@ class Profile {
   List<Post> _posts; // new from UML
   Map _weightLog = {DateTime: 0};
 
-  Profile(this._forUser, 
-    [
-      this._description,
-      this._quote,
-      this._avatarImage,
-      this._progress,
-      this._challenges,
-      this._weightLog
-    ]
-  );
+  _ProfileState(this._forUser);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Column(children: <Widget>[
+            _buildProfileInfo(),
+            _buildQuote(),
+            _buildDietInfo(),
+            _buildWeightGraph(),
+            _buildChallenges()
+          ]),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileInfo() {
+    return Row(
+      // Name, Profile Picture, and Lifter Type
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      "FIRST LAST",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w300,
+                          fontSize: 10,
+                          letterSpacing: 1),
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 80,
+                  )
+                ],
+              )),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            margin: EdgeInsets.only(top: 50),
+            child: Text("Lifter Type"),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildQuote() {
+    return Row(
+      // Motivational Quote
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(20),
+          child: Text('"THIS IS A MOTIVATIONAL QUOTE"'),
+        )
+      ],
+    );
+  }
+
+  Widget _buildDietInfo() {
+    return Row(
+      // Macros
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          // Calories Graph goes here
+          child: Icon(Icons.graphic_eq, size: 160),
+        ),
+        Expanded(
+          flex: 3,
+          child: Column(
+            // Macros
+            children: <Widget>[
+              Text("Protein:  "),
+              Text("Carbs:  "),
+              Text("Fats: "),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildWeightGraph() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(),
+        )
+      ],
+    );
+  }
+
+  Widget _buildChallenges() {
+    return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Row(
+          // Challenges
+          children: <Widget>[
+            Text(
+              "Challenges",
+              style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30,
+                  letterSpacing: 3),
+            ),
+            // List of challenges here (may need to use the ListBuilder Widget for this)
+          ],
+        ));
+  }
 
   void addPost(Post post) => _posts.add(post);
   void addFriend() {}
@@ -40,4 +165,5 @@ class Profile {
   void setDescription(String description) => _description = description;
   void setQuote(String quote) => _quote = quote;
   void updateProgress(double progress) => _progress = progress;  // unsure if this works
+  
 }
--- gym_space/lib/tabs/me.dart
+++ gym_space/lib/tabs/me.dart
@@ -1,131 +1,15 @@
 import 'package:flutter/material.dart';
 import 'widget_tab.dart';
+import 'package:GymSpace/logic/profile.dart';
+import 'package:GymSpace/logic/user.dart';
 
 class MeTab extends WidgetTab {
+  final User _me = new User("rollininrice", "rollininrice@gmail.com");
+
   MeTab(String title) : super(title, mainColor: Colors.green);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: <Widget>[
-          Column(children: <Widget>[
-            _buildProfileInfo(),
-            _buildQuote(),
-            _buildDietInfo(),
-            _buildWeightGraph(),
-            _buildChallenges()
-          ]),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildProfileInfo() {
-    return Row(
-      // Name, Profile Picture, and Lifter Type
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Container(),
-        ),
-        Expanded(
-          flex: 2,
-          child: Container(
-              margin: EdgeInsets.only(top: 20),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "FIRST LAST",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w300,
-                          fontSize: 10,
-                          letterSpacing: 1),
-                    ),
-                  ),
-                  CircleAvatar(
-                    radius: 80,
-                  )
-                ],
-              )),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            margin: EdgeInsets.only(top: 50),
-            child: Text("Lifter Type"),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildQuote() {
-    return Row(
-      // Motivational Quote
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(20),
-          child: Text('"THIS IS A MOTIVATIONAL QUOTE"'),
-        )
-      ],
-    );
-  }
-
-  Widget _buildDietInfo() {
-    return Row(
-      // Macros
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          // Calories Graph goes here
-          child: Icon(Icons.graphic_eq, size: 160),
-        ),
-        Expanded(
-          flex: 3,
-          child: Column(
-            // Macros
-            children: <Widget>[
-              Text("Protein:  "),
-              Text("Carbs:  "),
-              Text("Fats: "),
-            ],
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget _buildWeightGraph() {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(),
-        )
-      ],
-    );
-  }
-
-  Widget _buildChallenges() {
-    return Container(
-        margin: EdgeInsets.only(top: 20),
-        child: Row(
-          // Challenges
-          children: <Widget>[
-            Text(
-              "Challenges",
-              style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 30,
-                  letterSpacing: 3),
-            ),
-            // List of challenges here (may need to use the ListBuilder Widget for this)
-          ],
-        ));
+    return Profile(_me);
   }
 }
