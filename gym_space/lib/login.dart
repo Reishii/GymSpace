5ae0a4109dea9af7e75cadcdceedf0b38a5849b5
Reishii <ojiescala@gmail.com>
2019-02-25 02:34:09 -0800

    Refactored the code and added authentication

--- gym_space/lib/login.dart
+++ gym_space/lib/login.dart
@@ -1,13 +1,73 @@
 import 'package:flutter/material.dart';
+import 'auth.dart';
 
 class LoginPage extends StatefulWidget {
+  LoginPage({this.auth, this.onLoggedIn});
+  final BaseAuth auth;
+  final VoidCallback onLoggedIn;
   @override
   LoginPageState createState () => new LoginPageState();
-
 }
 
+enum FormType {
+  login,
+  register
+}
 class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
+  
+  final formKey = new GlobalKey<FormState>();
+  // User info
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
 
+
+  bool validateAndSave() {
+    final form = formKey.currentState;
+    if(form.validate()){
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  // Checks if User is valid from Firebase authentication 
+  void validateAndSubmit() async {
+    if(validateAndSave()) {
+      try {
+        if(_formType == FormType.login) {
+          String userID = await widget.auth.signInWithEmailAndPassword(_email, _password);   
+          print('Signed in: $userID');
+        }
+        else {
+          String userID = await widget.auth.createUserWithEmailAndPassword(_email, _password);
+          print('Registered User: $userID');
+        }
+        widget.onLoggedIn();
+      }
+      catch (e) {
+        print('Error: $e');
+      }
+    }
+  }
+
+  // Move buttons to show 'create an account'
+  void moveToRegister() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  // Move buttons to show 'Login'
+  void moveToLogin() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+  // Animates the GymSpace Icon
   AnimationController _iconAnimationController;
   Animation<double> _iconAnimation;
 
@@ -42,10 +102,11 @@ class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixi
           new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              new FlutterLogo( // just using Flutter Logo as a base. Will change to Gym Space Logo whenever
+              /*new FlutterLogo( // just using Flutter Logo as a base. Will change to Gym Space Logo whenever
                 size: _iconAnimation.value * 100,
-              ),
+              ),*/
               new Form(
+                key: formKey,
                 child: new Theme(
                   data: new ThemeData(
                     brightness: Brightness.dark,
@@ -57,36 +118,13 @@ class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixi
                       )
                     )
                   ),
-                  child: new  Container(
+                  child: new Container(
                     padding :const EdgeInsets.all(40.0),
                     child: new Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Enter Email",
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Enter Password",
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                          obscureText: true,
-                        ),
+                      children: buildInputs() + buildSubmitButtons() + <Widget>[
                         new Padding(
                           padding: const EdgeInsets.only(top: 20.0),
-                        ),
-                        new MaterialButton(
-                          height: 40.0,
-                          minWidth: 100.0,
-                          color: Colors.indigo[900],
-                          textColor: Colors.white,
-                          child: new Text("Login"),
-                          onPressed: () {
-                            Navigator.of(context).pushNamed('/home');
-                          } 
                         ),
                       ],
                     ),
@@ -98,5 +136,80 @@ class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixi
         ],
       ),
     );
+  }
+
+  // Gets Email and Passwords
+  List<Widget> buildInputs() {
+    return [
+      new TextFormField(
+        decoration: new InputDecoration(
+          labelText: "Enter Email",
+        ),
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) => value.isEmpty ? 'Error: Email is empty' : null,
+        onSaved: (value) => _email = value,
+      ),
+      new TextFormField(
+        decoration: new InputDecoration(
+          labelText: "Enter Password",
+        ),
+        keyboardType: TextInputType.text,
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Error: Password is empty' : null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  // The Recieve
+  List<Widget> buildSubmitButtons() {
+    if(_formType == FormType.login){
+      return [
+        new MaterialButton(
+          height: 40.0,
+          minWidth: 100.0,
+          color: Colors.indigo[900],
+          textColor: Colors.white,
+          child: new Text("Login"),
+          onPressed: () {
+            validateAndSubmit();
+          } 
+        ),
+        new MaterialButton(
+          height: 40.0,
+          minWidth: 200.0,
+          color: Colors.indigo[900],
+          textColor: Colors.white,
+          child: new Text("Register an account"),
+          onPressed: () {
+            moveToRegister();
+          }
+        )
+      ];
+    }
+    else {
+      return [
+        new MaterialButton(
+          height: 40.0,
+          minWidth: 100.0,
+          color: Colors.indigo[900],
+          textColor: Colors.white,
+          child: new Text("Create an account"),
+          onPressed: () {
+            validateAndSubmit();
+          } 
+        ),
+        new MaterialButton(
+          height: 40.0,
+          minWidth: 300.0,
+          color: Colors.indigo[900],
+          textColor: Colors.white,
+          child: new Text("Have an account? Login"),
+          onPressed: () {
+            moveToLogin();
+          }
+        )
+      ];
+    }
   }
 }
