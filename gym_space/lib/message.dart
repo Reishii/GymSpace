eb4af89ed1e4652b5a40e3c0529e50d92411aa95
pstanik <paulstanik3@gmail.com>
2019-02-26 15:24:12 -0800

    new messager dart file

---
+++ gym_space/lib/message.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';  
+import 'package:image_picker/image_picker.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:intl/intl.dart';
+
+
+
+// // Add the In-App Messaging dependency:
+// implementation 'com.google.firebase:firebase-inappmessaging-display:17.0.5'
+
+// // Check that your Firebase dependency is up-to-date:
+// implementation 'com.google.firebase:firebase-core:16.0.7'
+
+
+
