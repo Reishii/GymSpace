abc8fdfd2cd82cfbf43b24a1b7873aeb10076b9b
pstanik <paulstanik3@gmail.com>
2019-02-27 21:40:02 -0800

    chat button starting

--- gym_space/lib/message.dart
+++ gym_space/lib/message.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:shared_preferences/shared_preferences.dart';  
-import 'package:image_picker/image_picker.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:intl/intl.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:shared_preferences/shared_preferences.dart';  
+// import 'package:image_picker/image_picker.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:intl/intl.dart';
 
 
 
