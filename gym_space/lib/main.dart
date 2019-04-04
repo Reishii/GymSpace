0921907fff8997b865fda589c029c96004c3947c
Tungcab <tungcab@unlv.nevada.edu>
2019-04-03 19:51:32 -0700

    hopefully connecting database

--- gym_space/lib/main.dart
+++ gym_space/lib/main.dart
@@ -1,14 +1,26 @@
 // import 'home.dart';
-import 'logic/status.dart';
-import 'logic/auth.dart';
+import 'dart:async';
 import 'misc/colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:GymSpace/newHome.dart';
-import 'package:GymSpace/page/profile_page.dart';
-import 'package:GymSpace/widgets/app_drawer.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(GymSpace());
+
+Future<void> main() async{
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'gymspace',
+    options: const FirebaseOptions(
+      googleAppID: '1:936699691309:android:3aeae822367bc185',
+      apiKey: 'AIzaSyD-Q_wLERYdlEBK97oe3qdHz7BVGMRKxFY',
+      projectID: 'gymspace',
+    ),
+  );
+  final Firestore firestore = Firestore(app: app);
+  await firestore.settings(timestampsInSnapshotsEnabled: true);
+
+  runApp(GymSpace());
+}
 
 class GymSpace extends StatelessWidget {
   @override
