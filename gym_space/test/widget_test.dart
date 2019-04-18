9d7a39b99f7922289bc934a8ea051527cf4d961f
Rolly Lacap <lacaprjc@gmail.com>
2019-04-18 00:22:25 -0700

    Small fix for file removals.

--- gym_space/test/widget_test.dart
+++ gym_space/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:GymSpace/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(GymSpace());
+    await tester.pumpWidget(GymSpace(Scaffold()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
