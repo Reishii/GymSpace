62c34606328d57b34322f7ba618e5e3fdcf0aa2c
CoolComp <marc@sevilayha.com>
2019-05-02 14:37:44 -0700

    implemented nutrition_widget

--- gym_space/lib/page/me_page.dart
+++ gym_space/lib/page/me_page.dart
@@ -1,6 +1,7 @@
 import 'package:GymSpace/logic/user.dart';
 import 'package:GymSpace/page/nutrition_page.dart';
 import 'package:GymSpace/widgets/media_tab.dart';
+import 'package:GymSpace/widgets/nutrition_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:GymSpace/misc/colors.dart';
@@ -13,6 +14,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:GymSpace/widgets/nutrition_widget.dart';
 
 class MePage extends StatefulWidget {
 
@@ -643,7 +645,7 @@ class _MePageState extends State<MePage> {
           }   
         ));
       },
-      onLongPress:() {_updateNutritionInfo(context);},
+      onLongPress:() {NutritionWidget().updateNutritionInfo(context, _dietKey);},
     );
   }
 
@@ -1731,169 +1733,6 @@ class _MePageState extends State<MePage> {
 
   return "weekOf" + now.toString().substring(0,10);
 } 
-
-  void  _updateNutritionInfo(BuildContext context) async{
-      int protein, carbs, fats, currentCalories = 0, caloricGoal;
-      DocumentSnapshot macroDoc = await Firestore.instance.collection('users').document(DatabaseHelper.currentUserID).get();//await Firestore.instance.collection('user').document(DatabaseHelper.currentUserID);
-      Map<String, dynamic> macroFromDB = macroDoc.data['diet'].cast<String, dynamic>();
-      int caloriesGoal = macroDoc.data['caloricGoal'];
-      
-    showDialog<String>(
-      context: context,
-      //child: SingleChildScrollView(
-        //padding: EdgeInsets.all(5.0),
-        child: AlertDialog(
-        title: Text("Update your daily macros"),
-        contentPadding: const EdgeInsets.all(16.0),
-        content:  
-          Container(
-          //Row(
-          height: 350,
-          width: 350,
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-             Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Protein',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) {
-                  (text != null) ? protein = int.parse(text): protein = 0;
-                  (text != null) ? currentCalories += protein * 4: currentCalories += 0;
-                }
-              ),
-            ),
-            
-            Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Carbs',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  hintStyle: TextStyle(
-                    fontSize: 16.0,
-                    color: GSColors.darkBlue,
-                  ),
-                    contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) { 
-                  text != null ? carbs = int.parse(text) : carbs = 0;
-                  text != null ? currentCalories += carbs * 4 : currentCalories += 0;
-                }
-              ),
-            ),
-    
-            Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Fats',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  hintStyle: TextStyle(
-                    fontSize: 16.0,
-                    color: GSColors.darkBlue,
-                  ),
-                    contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) {
-                    text != null ? fats = int.parse(text) : fats = 0;
-                    text != null ? currentCalories += fats * 9 : currentCalories += 0;
-                }
-              ),
-            ),
-
-             Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Caloric Goal',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  hintStyle: TextStyle(
-                    fontSize: 16.0,
-                    color: GSColors.darkBlue,
-                  ),
-                    contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) {
-                    text != null ? caloricGoal = int.parse(text) : caloricGoal = -1;
-                }
-              )
-             )
-
-          ],
-        )),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('Cancel'),
-            onPressed: (){
-              currentCalories = 0;
-              Navigator.pop(context);
-            }
-          ),
-          FlatButton(
-            child: const Text('Save'),
-            onPressed: (){
-
-            if(protein == null)
-              protein = 0;
-            if(carbs == null)
-              carbs = 0;
-            if(fats == null)
-              fats = 0;
-            if(currentCalories == null)
-              currentCalories = 0;
-            if(caloricGoal == null)
-              caloricGoal = -1;
-
-            macroFromDB[_dietKey][0] += protein;
-            macroFromDB[_dietKey][1] += carbs;
-            macroFromDB[_dietKey][2] += fats;
-            macroFromDB[_dietKey][3] += currentCalories;
-            if(caloricGoal != -1)
-               Firestore.instance.collection('users').document(DatabaseHelper.currentUserID).updateData(
-              {'caloricGoal': caloricGoal});
-              // caloriesGoal = caloricGoal;
-              //macroFromDB[_dietKey][4] = caloricGoal;
-            
-            currentCalories = 0;
-
-            Firestore.instance.collection('users').document(DatabaseHelper.currentUserID).updateData(
-              {'diet': macroFromDB});
-            // Firestore.instance.collection('users').document(DatabaseHelper.currentUserID).updateData(
-            //   {'caloricGoal': caloriesGoal});
-            _buildNutritionInfo(context);
-
-            Navigator.pop(context);
-            }
-          )
-        ],
-      )
-    );
-  }
 
   void  _updateWeightInfo(BuildContext context) async{
       String currentWeight, startingWeight;
--- gym_space/lib/page/nutrition_page.dart
+++ gym_space/lib/page/nutrition_page.dart
@@ -1,5 +1,6 @@
 import 'package:GymSpace/logic/user.dart';
 import 'package:GymSpace/widgets/app_drawer.dart';
+import 'package:GymSpace/widgets/nutrition_widget.dart';
 import 'package:GymSpace/widgets/page_header.dart';
 import 'package:flutter/material.dart';
 import 'package:GymSpace/global.dart';
@@ -47,7 +48,7 @@ class _NutritionPage extends State<NutritionPage> {
           color: Colors.white
         ),
         backgroundColor: GSColors.purple,
-        onPressed: () => _updateNutritionInfo(context)
+        onPressed: () => NutritionWidget().updateNutritionInfo(context, _dietKey), 
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: _buildAppBar(),
@@ -792,163 +793,4 @@ class _NutritionPage extends State<NutritionPage> {
       ),
     ); 
   }
-
-  void  _updateNutritionInfo(BuildContext context) async{
-      int protein, carbs, fats, currentCalories = 0, caloricGoal;
-      DocumentSnapshot macroDoc = await Firestore.instance.collection('users').document(DatabaseHelper.currentUserID).get();//await Firestore.instance.collection('user').document(DatabaseHelper.currentUserID);
-      var macroFromDB = macroDoc.data['diet'];
-      
-    showDialog<String>(
-      context: context,
-        child: AlertDialog(
-        title: Text("Update your daily macros"),
-        contentPadding: const EdgeInsets.all(16.0),
-        content:  
-          Container(
-          //Row(
-          height: 200,
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-             Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Protein',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) {
-                  (text != null) ? protein = int.parse(text): protein = 0;
-                  (text != null) ? currentCalories += protein * 4: currentCalories += 0;
-                }
-              ),
-            ),
-            
-            Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Carbs',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  hintStyle: TextStyle(
-                    fontSize: 16.0,
-                    color: GSColors.darkBlue,
-                  ),
-                    contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) { 
-                  text != null ? carbs = int.parse(text) : carbs = 0;
-                  text != null ? currentCalories += carbs * 4 : currentCalories += 0;
-                }
-              ),
-            ),
-    
-            Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Fats',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  hintStyle: TextStyle(
-                    fontSize: 16.0,
-                    color: GSColors.darkBlue,
-                  ),
-                    contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) {
-                    text != null ? fats = int.parse(text) : fats = 0;
-                    text != null ? currentCalories += fats * 9 : currentCalories += 0;
-                }
-              ),
-            ),
-
-
-             Flexible(
-              child:  TextField(
-                keyboardType: TextInputType.number,
-                maxLines: 1,
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Caloric Goal',
-                  labelStyle: TextStyle(
-                    fontSize: 18.0,
-                    color: GSColors.darkBlue,
-                  ),
-                  hintStyle: TextStyle(
-                    fontSize: 16.0,
-                    color: GSColors.darkBlue,
-                  ),
-                    contentPadding: EdgeInsets.all(10.0)
-                ),
-                onChanged: (text) {
-                    text != null ? caloricGoal = int.parse(text) : caloricGoal = -1;
-                }
-              )
-             )
-
-          ],
-        )),
-        actions: <Widget>[
-          FlatButton(
-            child: const Text('Cancel'),
-            onPressed: (){
-              currentCalories = 0;
-              Navigator.pop(context);
-            }
-          ),
-          FlatButton(
-            child: const Text('Save'),
-            onPressed: (){
-
-            if(protein == null)
-              protein = 0;
-            if(carbs == null)
-              carbs = 0;
-            if(fats == null)
-              fats = 0;
-            if(currentCalories == null)
-              currentCalories = 0;
-            if(caloricGoal == null)
-              caloricGoal = -1;
-
-            macroFromDB[_dietKey][0] += protein;
-            macroFromDB[_dietKey][1] += carbs;
-            macroFromDB[_dietKey][2] += fats;
-            macroFromDB[_dietKey][3] += currentCalories;
-            if(caloricGoal != -1)
-              macroFromDB[_dietKey][4] = caloricGoal;
-
-            print("*******************************************************************************");
-            print(caloricGoal);
-            
-            currentCalories = 0;
-
-            Firestore.instance.collection('users').document(DatabaseHelper.currentUserID).updateData(
-              {'diet': macroFromDB});
-            _buildNutritionInfo(context);
-
-            Navigator.pop(context);
-            }
-          )
-        ],
-      )
-    );
-  }
-
 }
