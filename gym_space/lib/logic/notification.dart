a2e021dfc9d4602d42c35aee5e1968183da63973
Audie <ojiescala@gmail.com>
2019-05-06 20:27:35 -0700

    Added postID to respective notification database

--- gym_space/lib/logic/notification.dart
+++ gym_space/lib/logic/notification.dart
@@ -59,13 +59,14 @@ class Notifications{
   String route;
   String receiver;
   String sender;
-
+  String postID;
   Notifications({
     this.title,
     this.body,
     this.route,
     this.receiver,
-    this.sender
+    this.sender,
+    this.postID
   });
  factory Notifications.fromJSON(Map<dynamic, dynamic> json){
     return Notifications(
@@ -73,7 +74,8 @@ class Notifications{
      body: json['body'],
      route: json['route'],
      receiver: json['fcmToken'],
-     sender: json['sender']
+     sender: json['sender'],
+     postID: json['postID']
     );
   }
 }
